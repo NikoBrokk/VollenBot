3d6ff6c89047231cfaa8427233c321c461b934e6
NikoBrokk <nikolaibrokhaug@gmail.com>
2026-01-06 19:16:55 +0100

    bakgrunn

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,16 @@ import Chatbot from './components/Chatbot';
 
 export default function Home() {
   return (
-    <div style={{ minHeight: '100vh' }}>
+    <div 
+      style={{ 
+        minHeight: '100vh',
+        backgroundImage: 'url(/assets/mvp.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed'
+      }}
+    >
       <Chatbot />
     </div>
   );
